0a239be00aa1d31823b6807652cfc2643aca816a
Varkoff <virgileboris96@hotmail.fr>
2020-03-12 17:44:06 +0100

    finition funFact

--- funFacts.js
+++ funFacts.js
@@ -1,45 +1,22 @@
 var count = 0;
 
+var quotes = ["Quand Google ne trouve pas quelque chose, il demande à Chuck Norris.", "Jesus Christ est né en 1940 avant Chuck Norris.", "Chuck Norris ne se mouille pas, c'est l'eau qui se Chuck Norris.", "Chuck Norris peut supprimer la corbeille.", "Chuck Norris ne vit pas sur Terre, la Terre vit sous Chuck Norris.", "Quand un moustique pique Chuck Norris, c'est le moustique qui se gratte.", "Chuck Norris a déjà compté jusqu'à l'infini. Deux fois.", "Si Chuck Norris attaque l'Empire, l'Empire ne contre attaque pas.", "Chuck Norris a fini son forfait illimité.", "Un film de Bruce Lee montre Chuck Norris se faire battre par Bruce Lee. C'est là l'effet spécial le plus cher de toute l'histoire du cinéma."]
+var fact = document.getElementById("fact");
+/* setInterval(changeFact, 3000); */
 function changeFact() {
-    if (count < 9) {
-        count = count + 1;
-    } else {
+
+    count++;
+    if (count >= quotes.length) {
         count = 0;
     }
-    switch (count) {
-        case 0:
-            document.getElementById("fact").innerHTML = "Quand Google ne trouve pas quelque chose, il demande à Chuck Norris.";
-            break;
-        case 1:
-            document.getElementById("fact").innerHTML = "Jesus Christ est né en 1940 avant Chuck Norris.";
-            break;
-        case 2:
-            document.getElementById("fact").innerHTML = "Chuck Norris ne se mouille pas, c'est l'eau qui se Chuck Norris.";
-            break;
-        case 3:
-            document.getElementById("fact").innerHTML = "Chuck Norris peut supprimer la corbeille.";
-            break;
-        case 4:
-            document.getElementById("fact").innerHTML = "Chuck Norris ne vit pas sur Terre, la Terre vit sous Chuck Norris.";
-            break;
-        case 5:
-            document.getElementById("fact").innerHTML = "Quand un moustique pique Chuck Norris, c'est le moustique qui se gratte.";
-            break;
-        case 6:
-            document.getElementById("fact").innerHTML = "Chuck Norris a déjà compté jusqu'à l'infini.Deux fois.";
-            break;
-        case 7:
-            document.getElementById("fact").innerHTML = "Si Chuck Norris attaque l'Empire, l'Empire ne contre attaque pas.";
-            break;
-        case 8:
-            document.getElementById("fact").innerHTML = "Chuck Norris a fini son forfait illimité.";
-            break;
-        case 9:
-            document.getElementById("fact").innerHTML = "Un film de Bruce Lee montre Chuck Norris se faire battre par Bruce Lee. C'est là l'effet spécial le plus cher de toute l'histoire du cinéma.";
-            break;
-        default:
-            document.getElementById("fact").text = "error";
-            break;
+    fact.style.opacity = 0;
+    fact.style.height = "40px";
 
-    }
+    setTimeout(() => {
+        fact.style.height = "40px";
+        fact.innerHTML = quotes[count];
+        var heightOfP = (fact.scrollHeight) + "px";
+        fact.style.height = heightOfP;
+        fact.style.opacity = 1;
+    }, 500);
 }
